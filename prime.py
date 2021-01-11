5f76382f1da5a83afd0e4ffd4faefe18ff78727f
CrownCrafter <46600783+CrownCrafter@users.noreply.github.com>
2021-01-11 17:39:02 +0530

    Added prime.py

---
+++ prime.py
@@ -0,0 +1,13 @@
+x = int(input("Enter num"))
+i = 2
+while True:
+    div = x%i
+
+    if(i == x):
+        print("Prime")
+        break
+    if(div == 0):
+        print("Composite")
+        break
+    i += 1
+print("Execution complete")
